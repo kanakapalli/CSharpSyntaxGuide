ceb6f757ae8274e12b4f4c280d9ae65c8a1e0b12
KANAKAPALLI ANURAG <38519098+kanakapalli@users.noreply.github.com>
2023-09-28 07:56:41 +0530

    init

---
+++ C_Sharp_SyntaxAnalysis-main/helloworldtest/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using CsharpDemo;
+
+class Program
+{
+    static void Main(string[] arg)
+    {
+        Log("DataTypes.Demo", DataTypes.Demo);
+        Log(" Operators.Demo()",  Operators.Demo);
+        Log("Logging.Demo", Logging.Demo);
+        Log("CollectionExplanations.Demo", CollectionExplanations.Demo);
+        
+    }
+
+private    static void Log(string name, Action logAction)
+        {
+            Console.WriteLine("==============================================================================");
+            Console.WriteLine($"================================ {name} ================================");
+            Console.WriteLine("==============================================================================");
+            logAction();
+            Console.WriteLine("==============================================================================");
+            Console.WriteLine("==============================================================================");
+
+        }
+        
+
+}
+
+
+ 
+
